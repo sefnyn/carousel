b0f250109f41bc5a7b9d1ad32c2cd8ea4a8aa67b
ns <ns@bach.dur.ac.uk>
2019-10-07 16:09:33 +0100

    Updates

---
+++ precache-manifest.535718ea0df64085b315f0258b9c0217.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562cd021d5b6dd89c8caac0a4b2b7e6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87b0dbea80c470609e7d",
+    "url": "/static/js/2.0f0b6bf1.chunk.js"
+  },
+  {
+    "revision": "5508f43b88a0df8a5233",
+    "url": "/static/js/main.70c113ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dea2f5685c0c6e31ef32396454a9b2e6",
+    "url": "/static/media/slide001.dea2f568.png"
+  },
+  {
+    "revision": "bdad93546df4fd2eb26b2988ebfeb627",
+    "url": "/static/media/slide002.bdad9354.png"
+  },
+  {
+    "revision": "136f98160a013e818d61599fe313cac9",
+    "url": "/static/media/slide003.136f9816.png"
+  },
+  {
+    "revision": "0a81302df1299fb4746bf5682c7cbc7f",
+    "url": "/static/media/slide004.0a81302d.png"
+  },
+  {
+    "revision": "777c7bd926cf76495dc51693184a15d6",
+    "url": "/static/media/slide005.777c7bd9.png"
+  },
+  {
+    "revision": "1feca2e0a85ecd2ac6413d955adec177",
+    "url": "/static/media/slide006.1feca2e0.png"
+  },
+  {
+    "revision": "fd1255da57c4e4ce7a27d123b5a628ec",
+    "url": "/static/media/slide007.fd1255da.png"
+  }
+]);

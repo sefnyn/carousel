8b105a691422bd4034b56c98c0032f7a373d0af5
ns <ns@bach.dur.ac.uk>
2019-10-07 15:39:06 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Gallery, GalleryImage } from "react-gesture-gallery";
+
+const images = [
+  "https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80",
+  "https://images.unsplash.com/photo-1557389352-e721da78ad9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  "https://images.unsplash.com/photo-1553969420-fb915228af51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80",
+  "https://images.unsplash.com/photo-1550596334-7bb40a71b6bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  "https://images.unsplash.com/photo-1550640964-4775934de4af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+];
+
+function App() {
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    return
+  }, [index]);
+
+  return (
+    <Gallery
+      style={{
+        background: "black",
+        height: "100vh",
+        width: "100vw"
+      }}
+      index={index}
+      onRequestChange={i => {
+        setIndex(i);
+      }}
+    >
+      {images.map(image => (
+        <GalleryImage objectFit="contain" key={image} src={image} />
+      ))}
+    </Gallery>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
